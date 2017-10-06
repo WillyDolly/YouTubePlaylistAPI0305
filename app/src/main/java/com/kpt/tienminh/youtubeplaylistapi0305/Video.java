8c9d83374820f9c12e41681168a52c0186340bf2
WillyDolly <nguyenpk007@gmail.com>
2017-10-06 13:54:19 +0700

    Initial commit

---
+++ app/src/main/java/com/kpt/tienminh/youtubeplaylistapi0305/Video.java
@@ -0,0 +1,17 @@
+package com.kpt.tienminh.youtubeplaylistapi0305;
+
+/**
+ * Created by TienMinh on 6/28/2016.
+ */
+public class Video {
+
+    public String Title;
+    public String Thumnail;
+    public String IdVideo;
+
+    public Video(String title, String thumnail, String idVideo) {
+        Title = title;
+        Thumnail = thumnail;
+        IdVideo = idVideo;
+    }
+}
